86a39b168a3f51f8e3e163fba04ddb199fb8d7d7
alich6 <alicheraghi148@gmail.com>
2022-09-17 21:48:25 +0430

    add links to home page

--- src/pages/public/Home/Home.tsx
+++ src/pages/public/Home/Home.tsx
@@ -1,12 +1,18 @@
 import type { FC } from 'react'
+import { Link } from 'react-router-dom'
+import { DASHBOARD_ROUTE, LOGIN_ROUTE } from 'constants/routes'
+import { Container } from 'react-bootstrap'
 
 interface HomeProps {}
 
 const Home: FC<HomeProps> = () => {
 	return (
-		<div>
+		<Container>
 			<p>Home Page</p>
-		</div>
+			<Link to={LOGIN_ROUTE}>Login</Link>
+			<br />
+			<Link to={DASHBOARD_ROUTE}>Dashboard</Link>
+		</Container>
 	)
 }
 
